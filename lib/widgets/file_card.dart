9416fde676a21ef8ce7c575cd6172c34133e31dc
DealiAxy <dealiaxy@gmail.com>
2023-02-07 16:07:38 +0800

    ⚘支持多种文件图标显示

--- lib/widgets/file_card.dart
+++ lib/widgets/file_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mime/mime.dart';
 import 'package:dont_poop_in_my_phone/viewmodels/index.dart';
 
 class FileCard extends StatelessWidget {
@@ -11,12 +12,50 @@ class FileCard extends StatelessWidget {
     return GestureDetector(
       child: Card(
         child: ListTile(
-          leading: Icon(Icons.description_outlined, size: 40),
+          leading: _buildFileIcon(),
           title: Text(fileItem.fileName),
           subtitle: Text(fileItem.fileSize),
         ),
       ),
       onTap: fileItem.launch,
     );
+  }
+
+  Widget _buildFileIcon() {
+    final fullMime = lookupMimeType(fileItem.filepath) ?? 'unknown';
+    final tempArray = fullMime.split('/');
+    final majorMime = tempArray.isEmpty ? fullMime : tempArray[0];
+    var icon = Icons.description_outlined;
+    switch (majorMime) {
+      case 'application':
+        icon = Icons.settings_applications_outlined;
+        break;
+      case 'audio':
+        icon = Icons.audio_file_outlined;
+        break;
+      case 'chemical':
+        icon = Icons.schema_outlined;
+        break;
+      case 'example':
+        icon = Icons.explicit_outlined;
+        break;
+      case 'font':
+        icon = Icons.font_download_outlined;
+        break;
+      case 'image':
+        icon = Icons.image_outlined;
+        break;
+      case 'model':
+        icon = Icons.model_training_outlined;
+        break;
+      case 'text':
+        icon = Icons.text_format_outlined;
+        break;
+      case 'video':
+        icon = Icons.video_file_outlined;
+        break;
+    }
+
+    return Icon(icon, size: 40);
   }
 }
